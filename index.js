b418e4503d6629f1ebf4cddaac0007a48d3d366c
kush Muhamad <kushmohammed98@yahoo.com>
2023-03-20 07:43:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainElement = document.getElementById("main");
+if (mainElement) {
+  mainElement.remove();
+}
+
+const name = "Kush Muhamad"; 
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = `${name} is the champion`;
